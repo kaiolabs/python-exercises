d79604918beec73f6892b59f052d920aa638a7f4
Arcanodev <92180339+kaiolabs@users.noreply.github.com>
2022-03-06 15:26:32 -0300

    Update

--- Jogo - jan ken pon.py
+++ Jogo - jan ken pon.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import random
 import os
 
-=======
->>>>>>> 84a473dc8103bc1fb2e3272e5c545c762dbdddd3
 pedra = '''
     _______
 ---'   ____)
@@ -11,10 +8,7 @@ pedra = '''
       (_____)
       (____)
 ---.__(___)
-<<<<<<< HEAD
 
-=======
->>>>>>> 84a473dc8103bc1fb2e3272e5c545c762dbdddd3
 '''
 
 papel = '''
@@ -24,10 +18,7 @@ papel = '''
           _______)
          _______)
 ---.__________)
-<<<<<<< HEAD
 
-=======
->>>>>>> 84a473dc8103bc1fb2e3272e5c545c762dbdddd3
 '''
 
 tesoura = '''
@@ -37,7 +28,6 @@ tesoura = '''
        __________)
       (____)
 ---.__(___)
-<<<<<<< HEAD
 
 '''
 
@@ -73,28 +63,3 @@ while True:
     if(continuar == "N"):
         break
     os.system('cls')
-=======
-'''
-#Pedra ganha da tesoura (amassando-a ou quebrando-a).
-#Tesoura ganha do papel (cortando-o).
-#Papel ganha da pedra (embrulhando-a).
-
-import random
-
-escolha_maquina = random.randint(1,3)
-escolha_usuario = int(input("Vamos começar o jogo\n                        1-PEDRA, 2-PAPEL E 3-TESOURA.\nO que você escolhe? \n\n"))
-
-if escolha_usuario == escolha_maquina:
-  print("\nO JOGO EMPATOU!")
-elif (escolha_usuario == 1 and escolha_maquina == 3) or (escolha_usuario == 2 and escolha_maquina == 1) or(escolha_usuario == 3 and escolha_maquina == 2):
-     print("\nVOCÊ GANHOU!")
-else:
-  print("\nVOCÊ PERDEU!")
-
-print("\n\n")
-           #0       1       2
-opcoes = [pedra , papel, tesoura]
-print("Você:")
-print(f"{opcoes[escolha_usuario -1 ]}\nMáquina:{opcoes[escolha_maquina -1 ]}")
-
->>>>>>> 84a473dc8103bc1fb2e3272e5c545c762dbdddd3
